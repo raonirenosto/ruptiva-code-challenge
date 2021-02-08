539b27540b5aeb830587f3412890c4182d37ba66
Raoni Coelho <raonicoelho@gmail.com>
2021-02-08 14:58:11 -0300

    Create Main Screen

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const MainScreen = () => {
+  return <View>
+    <Text>MainScreen</Text>
+  </View>
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default MainScreen;
