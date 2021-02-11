35b705992040d563569de4d4f4c6b082cd318557
Raoni Renosto Coelho <raoni.renosto@evolua.digital>
2021-02-11 13:42:41 -0300

    Improve MainScreen

--- src/app/screens/MainScreen.js
+++ src/app/screens/MainScreen.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import UserForm from '../components/UserForm/UserForm';
-import UserList from '../components/UserList/UserList';
+import { View, StyleSheet } from 'react-native';
+import UserForm from '../components/UserForm';
+import UserList from '../components/UserList';
 
 const MainScreen = () => {
   return <View>
-    <Text style={styles.text}>Digite seus dados: </Text>
     <UserForm />
     <UserList />
   </View>
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 16,
-    margin: 10
-  }
+
 });
 
 export default MainScreen;
