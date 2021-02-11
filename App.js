6caa4d3dee0610a82ce381a77ab4e9d92d72a556
Raoni Coelho <raonicoelho@gmail.com>
2021-02-11 14:39:33 -0300

    Remove unused imports from App

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, LogBox } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import MainScreen from './src/app/screens/MainScreen'
-import { FirebaseInit } from './src/api/CloudFirestore';
 
 export default function App() {
-  LogBox.ignoreAllLogs(true)
   return (
     <View style={styles.container}>
       <MainScreen />
